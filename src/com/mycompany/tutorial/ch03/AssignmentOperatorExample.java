a2cce30184c3e7ae96dd2c657cab52454ee36a7a
bum2us <qka342@gmail.com>
2022-08-05 17:37:50 +0900

    java tutorial

---
+++ src/com/mycompany/tutorial/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,15 @@
+package com.mycompany.tutorial.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int result = 0;
+		result += 10;
+		result = result + 10;
+		
+		System.out.println(result);
+	}
+
+}
