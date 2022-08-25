f1d601ea9067f26b8a97718d67f41a8f77c3673e
bum2us <qka342@gmail.com>
2022-08-25 23:24:09 +0900

    substring test

---
+++ src/com/mycompany/tutorial/myStudy/tutorial_substring.java
@@ -0,0 +1,20 @@
+package com.mycompany.tutorial.myStudy;
+
+public class tutorial_substring {
+
+	public static void main(String[] args) {
+
+		String txt = "가족같은분위기의회사";
+		
+		System.out.println(txt);
+		
+		String sub1 = txt.substring(1, 4);
+		
+		System.out.println(sub1);
+		
+		String sub2 = txt.substring(8, 10);
+		
+		System.out.println(sub2);
+	}
+
+}
