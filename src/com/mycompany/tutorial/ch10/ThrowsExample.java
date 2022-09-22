50ea518f0f1024fd35b8231630b15850eaf49254
bum2us <qka342@gmail.com>
2022-09-22 19:09:51 +0900

    예외처리

---
+++ src/com/mycompany/tutorial/ch10/ThrowsExample.java
@@ -0,0 +1,15 @@
+package com.mycompany.tutorial.ch10;
+
+public class ThrowsExample {
+
+	public static void findClass() throws ClassNotFoundException {
+		
+		Class clazz = Class.forName("java.lang.String2");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//findClass();
+	}
+
+}
---
+++ src/com/mycompany/tutorial/ch10/TryCatchFinalExample.java
@@ -0,0 +1,14 @@
+package com.mycompany.tutorial.ch10;
+
+public class TryCatchFinalExample {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		// TODO Auto-generated method stub
+//		try {
+			Class clazz = Class.forName("java.lang.String2");
+//		} catch(ClassNotFoundException e) {
+//			System.out.println("클래스가 존재하지 않습니다.");
+//		}
+	}
+
+}
